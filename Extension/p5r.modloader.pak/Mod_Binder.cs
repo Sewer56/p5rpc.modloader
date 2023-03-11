d39f81af9f6bb024f20099bfa2afcf59a2e217d7
Sophia Treadwell <sophia.treadwell@jackpinetech.com>
2023-03-10 23:04:59 -0500

    Fixed: Small changes for PAK Merging

--- Extension/p5r.modloader.pak/Mod_Binder.cs
+++ Extension/p5r.modloader.pak/Mod_Binder.cs
@@ -1,14 +1,8 @@
 ﻿using System.Diagnostics;
-using System.Threading.Tasks;
 using CriFs.V2.Hook.Interfaces;
 using CriFs.V2.Hook.Interfaces.Structs;
-using CriFsV2Lib.Definitions.Utilities;
-using FileEmulationFramework.Lib;
-using p5r.modloader.pak.Template;
 using PAK.Stream.Emulator.Interfaces.Structures.IO;
 using Persona.Merger.Cache;
-using Persona.Merger.Patching.Tbl;
-using Persona.Merger.Patching.Tbl.Name;
 
 namespace p5r.modloader.pak;
 
@@ -23,49 +17,35 @@ public partial class Mod
         _createMergedFileCacheTask.Wait();
 
         bind = context;
-
         var input = _pakEmulator.GetEmulatorInput();
         var cpks = _criFsApi.GetCpkFilesInGameDir();
-        var criFsLib = _criFsApi.GetCriFsLib();
         var tasks = new List<ValueTask>();
         var watch = Stopwatch.StartNew();
 
-        _logger.Info("PLEASE GET HERE");
         var pathToFileMap = context.RelativePathToFileMap;
-        
-        foreach(RouteGroupTuple group in input)
+        foreach (RouteGroupTuple group in input)
         {
             var dir = group.Route;
-            var file = group.Files.Directory.FullPath;
+            string dirdir = Path.GetDirectoryName(dir) != null ? Path.GetDirectoryName(dir) : "" ;
+            if (dirdir.Contains('.'))
+            {
+                dir = dir.Substring(0, dir.IndexOf(Path.DirectorySeparatorChar, dir.IndexOf(".")));
+            }
             _logger.Info("Route: {0}", dir);
-            tasks.Add(CachePak(pathToFileMap, file, @"R2\" + dir, cpks));
+            tasks.Add(CachePak(pathToFileMap, @"R2\" + dir, cpks));
         }
     }
 
-    private async ValueTask CachePak(Dictionary<string, List<ICriFsRedirectorApi.BindFileInfo>> pathToFileMap, string filePath, string route, string[] cpks)
+    private async ValueTask CachePak(Dictionary<string, List<ICriFsRedirectorApi.BindFileInfo>> pathToFileMap, string route, string[] cpks)
     {
 
-        bool exists = pathToFileMap.TryGetValue(filePath, out var candidates);
-        
-        string pathInCpk= RemoveR2Prefix(route);
+        string pathInCpk = RemoveR2Prefix(route);
         if (!TryFindFileInAnyCpk(pathInCpk, cpks, out var cpkPath, out var cpkEntry, out int fileIndex))
         {
             _logger.Warning("Unable to find PAK in any CPK {0}", pathInCpk);
             return;
         }
-        /*
-        if (exists)
-        {
-            // Build cache key
-            var cacheKey = GetCacheKeyAndSources(filePath, candidates, out var sources);
-            if (_mergedFileCache.TryGet(cacheKey, sources, out var cachedFilePath))
-            {
-                _logger.Info("Loading Merged TBL {0} from Cache ({1})", filePath, cachedFilePath);
-                ReplaceFileInBinderInput(pathToFileMap, filePath, cachedFilePath);
-                return;
-            }
-        }
-        */
+        
         // Else Merge our Data
         // First we extract.
         await Task.Run(async () =>
@@ -83,7 +63,9 @@ public partial class Mod
             var item = await _mergedFileCache.AddAsync(cacheKey, sources, extractedFile.RawArray);
 
             string pakPath = Path.Combine(bind.BindDirectory, route);
-            Directory.CreateDirectory(Path.GetDirectoryName(pakPath));
+            string dirs = Path.GetDirectoryName(pakPath);
+            if (dirs != null)
+                Directory.CreateDirectory(Path.GetDirectoryName(pakPath));
             if (!_pakEmulator.TryCreateFromFileSlice(Path.Combine(_mergedFileCache.CacheFolder, item.RelativePath), 0, pathInCpk, pakPath))
             {
                 _logger.Error("Oops!");
@@ -92,7 +74,7 @@ public partial class Mod
 
 
             ReplaceFileInBinderInput(pathToFileMap, route, pakPath);
-            //_logger.Info("Cached to {0}.", item.RelativePath);
+            _logger.Info("File emulated at {0}.", pakPath);
 
         });
     }
@@ -105,7 +87,7 @@ public partial class Mod
             new()
             {
                 FullPath = newFilePath,
-                ModId = "p5r.modloader.pak",
+                ModId = "p5rpc.modloader.pak",
                 LastWriteTime = DateTime.UtcNow
             }
         };
@@ -115,11 +97,11 @@ public partial class Mod
     
     private bool TryFindFileInAnyCpk(string filePath, string[] cpkFiles, out string cpkPath, out CpkCacheEntry cachedFile, out int fileIndex)
     {
+        _logger.Info($"Found {filePath}");
         foreach (var cpk in cpkFiles)
         {
             cpkPath = cpk;
             cachedFile = _criFsApi.GetCpkFilesCached(cpk);
-
             if (cachedFile.FilesByPath.TryGetValue(filePath, out fileIndex))
                 return true;
         }
