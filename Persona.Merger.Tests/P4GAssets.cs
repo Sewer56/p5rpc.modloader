022f05fe0ec4e82dc1926c7bd936c7193202b07b
AnimatedSwine37 <24914353+AnimatedSwine37@users.noreply.github.com>
2023-08-12 10:33:26 +1000

    Update P4G Itemtbl test

--- Persona.Merger.Tests/P4GAssets.cs
+++ Persona.Merger.Tests/P4GAssets.cs
@@ -5,8 +5,10 @@ public static class P4GAssets
     public static readonly string AssetsFolder = Path.Combine(Path.GetDirectoryName(new Uri(AppContext.BaseDirectory).LocalPath)!, "Assets", "P4G");
     public static readonly string TempFolder = Path.Combine(Path.GetDirectoryName(new Uri(AppContext.BaseDirectory).LocalPath)!, "Temp");
 
-    public static readonly string ItemBefore = Path.Combine(AssetsFolder, "Item", "Before", "itemtbl.bin");
-    public static readonly string ItemAfter = Path.Combine(AssetsFolder, "Item", "After", "itemtbl.bin");
+    public static readonly string ItemOriginal = Path.Combine(AssetsFolder, "Item", "Original", "itemtbl.bin");
+    public static readonly string ItemMerged = Path.Combine(AssetsFolder, "Item", "Merged", "itemtbl.bin");
+    public static readonly string ItemEdited1 = Path.Combine(AssetsFolder, "Item", "Edited", "itemtbl1.bin");
+    public static readonly string ItemEdited2 = Path.Combine(AssetsFolder, "Item", "Edited", "itemtbl2.bin");
 
     public static readonly string MessageOriginal = Path.Combine(AssetsFolder, "Message", "Original", "MSG.TBL");
     public static readonly string MessageEdited1 = Path.Combine(AssetsFolder, "Message", "Edited", "MSG1.TBL");
